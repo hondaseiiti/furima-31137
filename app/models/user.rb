1011295b2a2034a31071cd0451dd0cff61406497
hondaseiiti <chengyibentian@gmail.com>
2020-11-08 15:41:02 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname,            presence: true
+  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana,    presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :first_name_kana,     presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :birth_day,           presence: true
+
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+  
+end
